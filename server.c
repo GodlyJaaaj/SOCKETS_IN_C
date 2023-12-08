8ce7fb6b8337d856a2188c1c01e9c7c9c5d7a1b2
Sébastien LUCAS <sebastien.lucas@epitech.eu>
2023-12-08 04:22:39 +0100

    add : whole server

--- server.c
+++ server.c
@@ -1,53 +1,19 @@
-#include "ncurses.h"
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include "string.h"
-#include "pthread.h"
-
-#define MAX_CLIENTS 10
-#define PORT 40000
-
-
-#define CLIENT_BUFFER 1024
-
-int active_connections = 0;
-
-struct player {
-    int id;
-    int pos_y, pos_x;
-    char character;
-    int color;
-};
-
-typedef struct player player_s;
-
-struct client {
-    int id;
-    char is_use;
-    int fd_socket;
-    struct sockaddr_in address;
-    void *data;
-};
-
-typedef struct client client_s;
+#include "my_sockets.h"
+#include "server.h"
+#include "client.h"
 
 client_s clients[MAX_CLIENTS];
-
-// 0 1 2 3 4 5 6 7
-
 
 void init_client_s_struct(int max_clients, client_s clients[max_clients])
 {
     for (int i = 0; i < max_clients; i++) {
-        //clients[i].is_use = 0;
-        //clients[i].fd_socket = 0;
-        //memset(&clients[i].socket, 0, sizeof(struct sockaddr_in));
         memset(&clients[i], 0, sizeof(client_s));
         clients[i].id = i;
     }
 }
 
-player_s *init_player_s_struct(int id, int pos_y, int pos_x, char character, int color)
+player_s *init_player_s_struct(int id, int pos_y,
+    int pos_x, char character, int color)
 {
     player_s *player = (player_s *) malloc(sizeof(player_s));
     if (!player) {
@@ -59,10 +25,8 @@ player_s *init_player_s_struct(int id, int pos_y, int pos_x, char character, int
     player->pos_x = pos_x;
     player->character = character;
     player->color = color;
-
     return player;
 }
-
 
 client_s *find_free_slot(int max_clients, client_s clients[max_clients])
 {
@@ -74,24 +38,63 @@ client_s *find_free_slot(int max_clients, client_s clients[max_clients])
     return NULL;
 }
 
-
 void *handle_client(void *data)
 {
-    client_s *client = (client_s *) data;
+    client_s *client = data;
     char buffer[CLIENT_BUFFER] = {0};
-
-    while(1) {
-        ssize_t n = recv(client->fd_socket, buffer, CLIENT_BUFFER, MSG_DONTWAIT); // -1 0
+    while (1) {
+        ssize_t n = recv(client->fd_socket, buffer, CLIENT_BUFFER,
+            MSG_DONTWAIT); // -1 0
         if (n == 0) {
             int temp_id = client->id;
-            free((client_s *)client->data);
+            free((client_s *) client->data);
             memset(client, 0, sizeof(client_s));
             client->id = temp_id;
             break;
         }
-        buffer[n] = 0;
+        if (n > 0) {
+            player_s *player = (player_s *) client->data;
+            memcpy(player, buffer, sizeof(player_s));
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (clients[i].is_use && player->id == clients[i].id) {
+                    memcpy(clients[i].data, player, sizeof(player_s));
+                }
+            }
+        }
     }
     return NULL;
+}
+
+void *send_all_datas(void *data)
+{
+    char buffer[CLIENT_BUFFER] = {0};
+    int player_count = 0;
+
+    while (1) {
+        player_count = 0;
+        memset(buffer, 0, CLIENT_BUFFER);
+        long offset = 0;
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i].is_use) {
+                player_count++;
+                player_s *player = (player_s *) clients[i].data;
+                if (player == NULL) {
+                    continue;
+                }
+                memcpy(buffer + offset, player, sizeof(player_s));
+                offset += sizeof(player_s);
+            }
+        }
+        if (player_count > 0) {
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (clients[i].is_use) {
+                    send(clients[i].fd_socket, buffer,
+                        sizeof(player_s) * player_count, 0);
+                }
+            }
+        }
+        usleep(100000);
+    }
 }
 
 int handle_server()
@@ -103,54 +106,77 @@ int handle_server()
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
-
     int addrlen = sizeof(address);
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+        sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-
     if (listen(server_fd, MAX_CLIENTS) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-
+    //thread that perpetually send all players data to all clients
+    pthread_t data_thread;
+    if (pthread_create(&data_thread, NULL, send_all_datas, NULL) != 0) {
+        perror("pthread_create");
+    } else {
+        pthread_detach(data_thread);
+    }
     while (1) {
         //printf("\nWaiting for a connection...\n");
         //mvprintw(0, 0, "Waiting for a connection...\n");
         //refresh();
-        int free_socket = find_free_slot(MAX_CLIENTS, clients)->id;
-        client_s *current_client = &clients[free_socket];
 
-        if ((current_client->fd_socket = accept(server_fd, (struct sockaddr_in *) &current_client->address, (socklen_t*)&addrlen)) < 0) {
+        //This is necessary to always give the lowest free slot to the new client
+        struct sockaddr_in temp_address;
+        int temp_fd_socket;
+        //
+
+        if ((temp_fd_socket = accept(server_fd,
+            (struct sockaddr *) &temp_address, (socklen_t *) &addrlen)) < 0) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
 
+        int free_socket = find_free_slot(MAX_CLIENTS, clients)->id;
+
+        clients[free_socket].fd_socket = temp_fd_socket;
+        memcpy(&clients[free_socket].address, &temp_address,
+            sizeof(struct sockaddr_in));
+
+        client_s *current_client = &clients[free_socket];
         current_client->is_use = 1;
-        current_client->data = init_player_s_struct(free_socket, 0, 0, '#', COLOR_BLACK);
+
+        srand(time(NULL));
+        //select a random color for the player
+        int color = rand() % 7 + 1;
+
+        //choose a random character for the player
+        char character = rand() % 93 + 33;
+
+        current_client->data = init_player_s_struct(free_socket, 0, 0, character,
+            color);
 
         //FIRST CONNECTION
-        send(current_client->fd_socket, (player_s *) current_client->data, sizeof(player_s), 0);
+        send(current_client->fd_socket, (player_s *) current_client->data,
+            sizeof(player_s), 0);
 
         pthread_t thread;
-
-        if (pthread_create(&thread, NULL, handle_client, &current_client) != 0) {
+        if (pthread_create(&thread, NULL, handle_client,
+            (void *) current_client) != 0) {
             perror("pthread_create");
         } else {
             pthread_detach(thread);
@@ -167,18 +193,19 @@ int handle_server()
                 current++;
             }
         }
+        printf("\n\n\n\n");
         //refresh();
     }
 }
 
-
-
 int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(84);
+    }
+    PORT = atoi(argv[1]);
     init_client_s_struct(MAX_CLIENTS, clients);
-
-    //client_s *my_free_slot = find_free_slot(MAX_CLIENTS, clients);
-
     handle_server();
     return 0;
 }
